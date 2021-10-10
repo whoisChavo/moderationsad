033775b6a3f0c7a341eaa253ab83dc9d54e0dfce
Chavo but Happy <89283086+whoisChavo@users.noreply.github.com>
2021-10-10 12:10:55 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,60 @@
+module.exports = {
+   "token":"ODkxNjE1MTM3MTg4OTUwMDE2.YVA7Tg.dHFwCyvig8Xw6TuKHJ0q02KkI8s",
+    bot: {
+        botOwner: ["839665290882449428"],
+        botPrefix: "!",
+        botStatus: "Châvø ❤️ Maee",
+        botVoice: "896493419453612171",
+        mongoURL: "mongodb://nurumm:nurumm@cluster0-shard-00-00.is0vl.mongodb.net:27017,cluster0-shard-00-01.is0vl.mongodb.net:27017,cluster0-shard-00-02.is0vl.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-2jbqci-shard-0&authSource=admin&retryWrites=true&w=majority",
+    },
+
+    roles: {
+        muteStaff: ["896493419415887941"],
+        vmuteStaff: ["896493419415887941"],
+        jailStaff: ["896493419386536021"],
+        banStaff: ["896493419386536022"],
+        muteRole: "896493419415887935",
+        vMuteRole: "896493419415887934",
+        jailRole: "896493419386536019",
+        unregisterRoles: ["896493419386536020"],
+        tagRole: "896493419415887939",
+        vipRole: "889199607723217003",
+        boosterRole: "",
+        enAltYt: "896493419415887939",
+        katıldı: "",
+        yasaklıTag: ""
+    },
+
+    channels: {
+        genelChat: "889199608763412495",
+        tagLog: "896493421403992143",
+        muteLog: "896493421626261511",
+        vMuteLog: "896493421626261512",
+        jailLog: "896493421806633010",
+        banLog: "896493421626261513",
+        messageLog: "896493421626261505",
+        meeting: "",
+        rolLog: "896493421626261504",
+        voiceLog: "896493421626261507",
+        yasaklıtagLog: "",
+        pubCategoryID: "896493420799987754",
+
+    },
+
+    guild: {
+        guildID: "896493419361353748",
+        tagges: ["♆"], // buraya taglarınızı giriyorsunuz etiket için: #0001 olarak
+        nameTag: "♆", // buraya tagı varsa ismin başına gelcek tag
+        defaultTag: "", // burda tagı yoksa ismin başına gelcek tag
+        banLimit: 7
+
+    },
+
+    emojis: {
+        yes: "<a:onay:896552109871333467>", // EMOJİLERİN SADECE ID GİRECEKSİNİZ 
+        no: "<a:red:896552250598629417>",
+        on: "<a:onay:896552109871333467>",
+        off: "<a:red:896552250598629417>"
+    }
+
+};
