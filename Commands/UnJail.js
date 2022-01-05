41e08e1d97dc519de27a446ce572e315350d30e9
Chavo but Happy <89283086+whoisChavo@users.noreply.github.com>
2022-01-05 15:30:35 +0300

    Update UnJail.js

--- Commands/UnJail.js
+++ Commands/UnJail.js
@@ -1,46 +1,79 @@
-const Discord = require("discord.js")
-const { Database, Logger } = require("../helpers/functions");
-const ayar = require('../settings.js')
-const penalData = require('../models/penal.js')
-const ms = require('ms');
+const { MessageEmbed } = require('discord.js')
+const datab = require('quick.db')
 const moment = require('moment')
-require('moment-duration-format')
-moment.locale('tr');
-module.exports.run = async(client, message, args, embed) => {
-    if (ayar.roles.jailStaff.some(s => !message.member.roles.cache.has(s)) && !message.member.hasPermission(8)) return message.channel.send(embed.setDescription(`${message.author}, Bu komutu kullanmak için yeterli yetkiye sahip değilsin!`)).sil(7);
-    let member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
-    if (!member) return message.channel.send(embed.setDescription(`${message.member}, Hatalı kullanım. Örnek: **${ayar.bot.botPrefix}unjail @Cross/ID**`)).sil(7)
+const ayarlar = require('../settings.js');
+exports.run = async (client, message, args) => {
 
-    let memberData = await penalData.find({ guildID: message.guild.id, userID: member.id })
-    let durumData = await penalData.find({ guildID: message.guild.id, userID: member.id, Tip: "JAIL" });
-    let durum = durumData.some(s => s.Durum == false);
-    if (durum || !durumData.length) return message.channel.send(embed.setDescription(`${member}, Adlı kullanıcının aktif bir **[JAIL]** cezası bulunmamakta!`)).sil(7);
+const permError = new MessageEmbed()
+    .setColor('RED')
+    .setTitle('Başarısız')
+    .setAuthor(message.author.tag, message.author.avatarURL({ size:1024, dynamic:true, format: "png"}))
+    .setDescription(`Bu Komutu Kullanmak İçin <@&${ayarlar.roles.jailStaff}> Yetkisi ve Üstü Bir Yetkiye Sahip Olmalısın!`)
 
-
-    if (member.id == message.author.id) return message.react(ayar.emojis.no)
-    if (member.id == client.user.id) return message.react(ayar.emojis.no);
-    if (member.user.bot) return message.react(ayar.emojis.no);
-    if (member.permissions.has(8)) return message.channel.send(embed.setDescription(`${message.member}, Yönetici bir kullanıcıya karışamam.`)).sil(7)
-    if (member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(embed.setDescription(`${message.member}, Bu kullanıcı sizden üst/aynı pozisyonda.`)).sil(7)
-
-    durumData.forEach(async(d, index) => {
-        if (d.Durum == true) {
-            member.roles.set(ayar.roles.unregisterRoles).catch(err => console.log('rol verme hatası'))
-            if (client.channels.cache.get(ayar.channels.jailLog)) client.channels.cache.get(ayar.channels.jailLog).send(embed.setDescription(`${member}, Adlı kullanıcının karantina cezası kaldırıldı.`))
-            d.Durum = false;
-            d.save();
-        } else {
-            return;
-        }
-    });
+    if(!message.member.roles.cache.has(ayar.roles.jailStaff)) return message.channel.send(permError);
 
 
+    const cezalırol = message.guild.roles.cache.find(r => r.id === ayarlar.roles.jailRole)
+    const jaillog = message.guild.channels.cache.find(c => c.id === ayarlar.channels.jailLog)
 
-};
+let kullanici = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
+let sebep = args[1]
+if(!kullanici) return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Bir kullanıcı etiketlemelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(!sebep) return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Bir sebep belirtmelisin.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(message.member.roles.highest.position <= kullanici.roles.highest.position) return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Etiketlenen kullanıcı sizden üst/aynı pozisyondadır.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(!kullanici.bannable)return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Etiketlenen kullanıcıya komutu kullanamazsın.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(kullanici.id === message.author.id)return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Kendine komutu kullanamazsın.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(kullanici.id === client.user.id)return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Bir botu komutu kullanamazsın.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+if(kullanici.id === message.guild.OwnerID) return message.channel.send(new MessageEmbed().setDescription(`${message.author}, Sunucu sahibinine komutu kullanamazsın.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
+
+let zaman1 = args[1]
+.replace("sn", "s")
+.replace("dk", "m")
+.replace("sa", "h")
+.replace("gün", "d");
+var vakit = zaman1
+.replace("m", " dakika")
+.replace("s", " saniye")
+.replace("h", " saat")
+.replace("d", " d");
+
+datab.delete(`cezali_${message.guild.id + kullanici.id}`, 'cezali')
+datab.delete(`süreJail_${message.mentions.users.first().id + message.guild.id}`, zaman1)
+
+let tumaylar = {
+"01": "Ocak",
+"02": "Şubat",
+"03": "Mart",
+"04": "Nisan",
+"05": "Mayıs",
+"06": "Haziran",
+"07": "Temmuz",
+"08": "Ağustos",
+"09": "Eylül",
+"10": "Ekim",
+"11": "Kasım",
+"12": "Aralık",
+}
+let aylar = tumaylar;
+
+
+moment.locale("tr");
+jaillog.send(new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('RANDOM').setTimestamp().setDescription(`**Cezası Bitirildi !**\n**Yetkili:** ${message.author} (\`${message.author.id}\`)\n**Kullanıcı:** ${kullanici.user} (\`${kullanici.user.id}\`)\n**Sebep:** \`${sebep}\` \n**Tarih:** \`${moment(Date.now()).add(10,"hours").format("HH:mm:ss DD MMMM YYYY")}\``));
+message.react('✅')
+
+kullanici.roles.remove(cezalırol)
+message.guild.roles.cache.forEach(async r => {
+let roller = datab.fetch(`${message.guild.id}.jail.${kullanici.id}.roles.${r.id}` )
+if(roller != r.id)  return ;
+if(roller){kullanici.roles.add(roller)}
+})
+
+
+}
 exports.config = {
     name: "unjail",
-    usage: `${ayar.bot.botPrefix}unjail [@Chavo/ID]`,
+    usage: `${ayarlar.bot.botPrefix}unjail [@Kullanıcı/ID] [Sebeb]`,
     guildOnly: true,
-    aliases: ["unjailmute"],
+    aliases: ["unjail"],
     cooldown: 3000
-};
+}
