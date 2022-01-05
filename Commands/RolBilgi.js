5219606093e1c1961990bdef66817380d46f3221
Chavo but Happy <89283086+whoisChavo@users.noreply.github.com>
2022-01-05 15:31:01 +0300

    Update RolBilgi.js

--- Commands/RolBilgi.js
+++ Commands/RolBilgi.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js")
 const { Database, Logger } = require("../helpers/functions");
 const ayar = require('../settings.js')
 module.exports.run = async(client, message, args, embed) => {
-        if (!message.member.hasPermission(8)) return message.channel.send(embed.setDescription(`${message.author}, Bu komutu kullanmak için yeterli yetkiye sahip değilsin!`)).sil(7);
-        let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.guild.roles.cache.find(s => s.name.toLowerCase().includes(args.slice(0).join(' ')))
+    if (!message.member.roles.cache.has(ayar.roles.kurucu)) return message.channel.send(embed.setDescription(`${message.author}, Bu komutu kullanmak için <@&${ayar.roles.kurucu}> yetkisine sahip olmalısın!`)).sil(7);   
+    let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.guild.roles.cache.find(s => s.name.toLowerCase().includes(args.slice(0).join(' ')))
 
-        if (!role) return message.channel.send(embed.setDescription(`${message.member}, Geçerli bir rol belirtmelisin.`)).sil(7);
+        if (!role) return message.channel.send(embed.setDescription(`**${message.member}, Geçerli bir rol belirtmelisin.**`)).sil(7);
 
         let uyeler = role.members;
         let sestekiler = role.members.filter(s => s.voice.channel);
@@ -13,7 +13,7 @@ module.exports.run = async(client, message, args, embed) => {
         let taglilar = role.members.filter(s => ayar.guild.tagges.some(a => s.user.tag.toLowerCase().includes(a)));
         let rolrenk = role.hexColor;
 
-        message.channel.send(`
+        message.channel.send(`**
 - ${role} rol bilgileri;
 - Rol Rengi: \`${rolrenk}\`
 - Rol ID: \`${role.id}\`
@@ -21,11 +21,11 @@ module.exports.run = async(client, message, args, embed) => {
 ─────────────────
 - Roldeki kişiler;
 ${uyeler.map(s => `${s} - (\`${s.id}\`)`).join('\n')}
-    `, {split: true})
+    **`, {split: true})
 };
 exports.config = {
     name: "rolbilgi",
-    usage: `${ayar.bot.botPrefix}rolbilgi [@Role/ID/Name]`,
+    usage: `${ayar.bot.botPrefix}rolbilgi [@Rol/ID/İsmi]`,
     guildOnly: true,
     aliases: ["rbilgi"],
     cooldown: 3000
