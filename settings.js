ca5a9271613dfd65700076cd64b3da8c06551868
Chavo but Happy <89283086+whoisChavo@users.noreply.github.com>
2022-01-05 15:11:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,52 +1,48 @@
 module.exports = {
-   "token":"ODkxNjE1MTM3MTg4OTUwMDE2.YVA7Tg.JAh9v5IZTlXZxvXUwsojQ5uVnn4",
     bot: {
         botOwner: ["839665290882449428"],
-        botPrefix: ".",
-        botStatus: "Châvø ❤️ Maee",
+        botToken: "ODkxNjE1MTM3MTg4OTUwMDE2.YVA7Tg._oyMqIcCyJuzHTM1cpVjsYDuRhw",
+        botPrefix: "!",
+        botStatus: "Chavo ❤️ Burjuva",
         botVoice: "896493419453612171",
-        mongoURL: "",
+        mongoURL: "mongodb://kanalguard123:123456789aqwe@cluster0-shard-00-00.2wavb.mongodb.net:27017,cluster0-shard-00-01.2wavb.mongodb.net:27017,cluster0-shard-00-02.2wavb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-natysk-shard-0&authSource=admin&retryWrites=true&w=majority",
     },
+    
 
     roles: {
-        muteStaff: ["896493419415887941"],
-        vmuteStaff: ["896493419415887941"],
-        jailStaff: ["896493419386536021"],
-        banStaff: ["896493419386536022"],
+ jailStaff: "896493419386536021",
+        banStaff: "896493419386536022",
         muteRole: "896493419415887935",
         vMuteRole: "896493419415887934",
         jailRole: "896493419386536019",
         unregisterRoles: ["896493419386536020"],
-        tagRole: "896493419415887939",
-        vipRole: "889199607723217003",
-        boosterRole: "",
-        enAltYt: "896493419415887939",
-        katıldı: "",
-        yasaklıTag: ""
+        vipRole: "896493419415887938",
+        boosterRole: "897889663350628414",
+        enAltYt: "896493419415887941",
+        yasaklıTag: "878668858456682516"
     },
-
     channels: {
-        genelChat: "889199608763412495",
-        tagLog: "896493421403992143",
+        genelChat: "908734497195954236",
+        tagLog: "906845906614255626",
         muteLog: "896493421626261511",
         vMuteLog: "896493421626261512",
-        jailLog: "896493421806633010",
+        jailLog: "928237707036078120",
         banLog: "896493421626261513",
-        messageLog: "896493421626261505",
-        meeting: "",
-        rolLog: "896493421626261504",
-        voiceLog: "896493421626261507",
+        messageLog: "906845834228932618",
+        rolLog: "906866982043922482",
+        voiceLog: "906845448634003476",
         yasaklıtagLog: "",
         pubCategoryID: "896493420799987754",
 
     },
 
+
     guild: {
         guildID: "896493419361353748",
-        tagges: ["⟡"], // buraya taglarınızı giriyorsunuz etiket için: #0001 olarak
+        tagges: [""], // buraya taglarınızı giriyorsunuz etiket için: #0001 olarak
         nameTag: "⟡", // buraya tagı varsa ismin başına gelcek tag
-        defaultTag: "", // burda tagı yoksa ismin başına gelcek tag
-        banLimit: 7
+        defaultTag: "•", // burda tagı yoksa ismin başına gelcek tag
+        banLimit: 5
 
     },
 
